699ecf92ff63a9ec4e6571359301273f4dcd71e7
medicendav <davidtorrezreyes@gmail.com>
2020-12-05 09:38:54 -0600

    Creando modulo procesos

---
+++ scripts/procesos/comandos.py
@@ -0,0 +1,47 @@
+"""
+Modulo: procesos.comandos
+Este modulo contiene funciones para leer y obtener la salida de retorno
+de un comando de shell
+"""
+import subprocess
+
+
+def cmd_output(cmd: str) -> str:
+    """Devueve una cadena con la salida de un comando de shell.
+    Si el comando no es valido, devuelve un string vacío.
+    Parametros:
+        cmd -> str: Comando a ejecutar
+    Return:
+        str: Candena con la salida del comando.
+    """
+    output = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        shell=True,
+        text=True,
+    )
+    if output.returncode == 0:
+        return output.stdout[:-1]
+    else:
+        return ""
+
+
+def execute(cmd: str) -> bool:
+    """Devueve una Booleano con la salida de un comando de shell.
+    Parametros:
+        cmd -> str: Comando a ejecutar
+    Return:
+        bool: Estado de ejecución del comando.
+    """
+    output = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        shell=True,
+        text=True,
+    )
+    if output.returncode == 0:
+        return True
+    else:
+        return False
